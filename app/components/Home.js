f16bcde86f90b84740237be3a6628f2044607d04
Kamil Mrzygłód <kamz@oticon.com>
2017-06-29 13:29:16 +0200

    boilerplate: Added an Electron boilerplate using electron-react-boilerplate

---
+++ app/components/Home.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.container} data-tid="container">
+          <h2>ARMata</h2>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ app/menu.js
@@ -0,0 +1,108 @@
+// @flow
+import { app, Menu, shell, BrowserWindow } from 'electron';
+
+export default class MenuBuilder {
+  mainWindow: BrowserWindow;
+
+  constructor(mainWindow: BrowserWindow) {
+    this.mainWindow = mainWindow;
+  }
+
+  buildMenu() {
+    if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+      this.setupDevelopmentEnvironment();
+    }
+
+    let template;
+    template = this.buildDefaultTemplate();
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    return menu;
+  }
+
+  setupDevelopmentEnvironment() {
+    this.mainWindow.openDevTools();
+    this.mainWindow.webContents.on('context-menu', (e, props) => {
+      const { x, y } = props;
+
+      Menu
+        .buildFromTemplate([{
+          label: 'Inspect element',
+          click: () => {
+            this.mainWindow.inspectElement(x, y);
+          }
+        }])
+        .popup(this.mainWindow);
+    });
+  }
+
+  buildDefaultTemplate() {
+    const templateDefault = [{
+      label: '&File',
+      submenu: [{
+        label: '&Open a template',
+        accelerator: 'Ctrl+O'
+      }, {
+        label: '&Close',
+        accelerator: 'Ctrl+W',
+        click: () => {
+          this.mainWindow.close();
+        }
+      }]
+    }, {
+      label: '&View',
+      submenu: (process.env.NODE_ENV === 'development') ? [{
+        label: '&Reload',
+        accelerator: 'Ctrl+R',
+        click: () => {
+          this.mainWindow.webContents.reload();
+        }
+      }, {
+        label: 'Toggle &Full Screen',
+        accelerator: 'F11',
+        click: () => {
+          this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+        }
+      }, {
+        label: 'Toggle &Developer Tools',
+        accelerator: 'Alt+Ctrl+I',
+        click: () => {
+          this.mainWindow.toggleDevTools();
+        }
+      }] : [{
+        label: 'Toggle &Full Screen',
+        accelerator: 'F11',
+        click: () => {
+          this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+        }
+      }]
+    }, {
+      label: 'Help',
+      submenu: [{
+        label: 'Learn More',
+        click() {
+          shell.openExternal('https://github.com/ARMataTeam/ARMata');
+        }
+      }, {
+        label: 'Documentation',
+        click() {
+          shell.openExternal('https://github.com/ARMataTeam/ARMata/wiki');
+        }
+      }, {
+        label: 'Community Discussions',
+        click() {
+          shell.openExternal('https://gitter.im/ARMataTeam/Lobby?utm_source=share-link&utm_medium=link&utm_campaign=share-link');
+        }
+      }, {
+        label: 'Search Issues',
+        click() {
+          shell.openExternal('https://github.com/ARMataTeam/ARMata/issues');
+        }
+      }]
+    }];
+
+    return templateDefault;
+  }
+}
