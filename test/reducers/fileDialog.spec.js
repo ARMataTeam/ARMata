f37a9068349605fa94575c52f449c761a5990edb
Kamil Mrzygłód <kamz@oticon.com>
2017-07-07 09:34:58 +0200

    #11 - fixed `Structure` structure, added simple reducer tests for `fileDialog` reducer

---
+++ test/reducers/fileDialog.spec.js
@@ -0,0 +1,14 @@
+import fileDialog from '../../app/reducers/fileDialog';
+import { OPEN_FILE } from '../../app/actions/fileDialog';
+
+describe('reducers', () => {
+  describe('fileDialog', () => {
+    it('should handle initial state', () => {
+      expect(fileDialog(undefined, {})).toMatchSnapshot();
+    });
+
+    it('should handle OPEN_FILE', () => {
+      expect(fileDialog({ selectedFilename: 'C:\\Temp\\Foo.json', fileData: '{"foo":"bar"}' }, { type: OPEN_FILE })).toMatchSnapshot();
+    });
+  });
+});
