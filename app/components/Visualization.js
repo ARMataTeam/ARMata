182de442324d5628eb9bb322ce737eef7381ee33
Kamil Mrzygłód <kamz@oticon.com>
2017-07-10 23:08:25 +0200

    #15 - eslint fixes

--- app/components/Visualization.js
+++ app/components/Visualization.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-
-import Graph from 'react-graph-vis'
+import Graph from 'react-graph-vis';
 
 export default class Visualization extends Component {
   props: {
@@ -13,13 +12,13 @@ export default class Visualization extends Component {
   render() {
     this.resources = this.props.json.resources;
 
-    let nodes = [];
-    for (var i = 0; i < this.resources.length; i++) {
-      nodes.push({ id: i, label: this.resources[i].name });
+    const resources = [];
+    for (let i = 0; i < this.resources.length; i += 1) {
+      resources.push({ id: i, label: this.resources[i].name });
     }
 
-    var graph = {
-      nodes: nodes,
+    const graph = {
+      nodes: resources,
       edges: [
         { from: 1, to: 2 },
         { from: 1, to: 3 },
@@ -28,22 +27,16 @@ export default class Visualization extends Component {
       ]
     };
 
-    var options = {
+    const options = {
       layout: {
         hierarchical: false
       },
       edges: {
-        color: "#FFFFFF"
+        color: '#FFFFFF'
       },
       autoResize: true
     };
 
-    var events = {
-      select: function (event) {
-        var { nodes, edges } = event;
-      }
-    }
-
-    return <Graph graph={graph} options={options} events={events} style={{ width: '100%', height: '100%' }} />;
+    return <Graph graph={graph} options={options} style={{ width: '100%', height: '100%' }} />;
   }
 }
