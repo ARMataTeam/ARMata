988bb5406132e8ce5c2f7302772d745ced384b9f
Kamil Mrzygłód <kamz@oticon.com>
2017-07-11 12:39:30 +0200

    #2 - fixed styles so everything is displayed correctly

--- app/components/Visualization.js
+++ app/components/Visualization.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import Graph from 'react-graph-vis';
+import styles from './Visualization.css'; // eslint-disable-line flowtype-errors/show-errors
 
 export default class Visualization extends Component {
   props: {
@@ -62,6 +63,6 @@ export default class Visualization extends Component {
       autoResize: true
     };
 
-    return <Graph graph={graph} options={options} style={{ width: '100%', height: '100%' }} />;
+    return (<div className={styles.visualization}><Graph graph={graph} options={options} style={{ width: '100%', height: '100%' }} /></div>);
   }
 }
