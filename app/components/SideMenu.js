1ed066117bc65f1a072750e23da070c6a99becf9
Kamil Mrzygłód <kamz@oticon.com>
2017-08-01 13:50:20 +0200

    #52 - temporarly removed `JsonPreview` component, which will be refactored and introduced as a different feature

--- app/components/SideMenu.js
+++ app/components/SideMenu.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react';
 import { List, ListItem } from 'material-ui/List';
 import ContentPaste from 'material-ui/svg-icons/content/content-paste';
-import Subject from 'material-ui/svg-icons/action/subject';
 import { grey100, grey500, grey50 } from 'material-ui/styles/colors';
 import { Link } from 'react-router-dom';
 import styles from './SideMenu.css'; // eslint-disable-line flowtype-errors/show-errors
@@ -18,10 +17,7 @@ export default class SideMenu extends Component {
       <div className={styles.sideMenu}>
         <List>
           <ListItem className={styles.menuItemIcon} onTouchTap={() => this.props.changeView('Structure')}>
-            <Link to="/Structure"><ContentPaste color={this.props.currentView === 'Structure' ? grey50 : grey500} hoverColor={grey100} /></Link>
-          </ListItem>
-          <ListItem className={styles.menuItemIcon} onTouchTap={() => this.props.changeView('JsonView')}>
-            <Link to="/JsonPreview"><Subject color={this.props.currentView === 'JsonView' ? grey50 : grey500} hoverColor={grey100} /></Link>
+            <Link to="/"><ContentPaste color={this.props.currentView === 'Structure' ? grey50 : grey500} hoverColor={grey100} /></Link>
           </ListItem>
         </List>
       </div>);
