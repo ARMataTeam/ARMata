06fe4833940194e0413c00e18b2c1e0817ea09e6
Kamil Mrzygłód <kamz@oticon.com>
2017-08-01 12:48:39 +0200

    #54 - eslint fixes

--- test/parsers/templateParser.spec.js
+++ test/parsers/templateParser.spec.js
@@ -10,11 +10,10 @@ describe('parsers', () => {
 
       expect(result.schema).toBe('some_schema');
       expect(result.contentVersion).toBe('1.0.0.0');
-      //expect(result.resources).toBe(json.resources);
     });
 
     it('should handle variable in resource name', () => {
-      const json = { $schema: 'some_schema', contentVersion: '1.0.0.0', resources: [{ name: "[variables('virtualMachineName')]" }], variables: { virtualMachineName: "VM-MultiNic" } };
+      const json = { $schema: 'some_schema', contentVersion: '1.0.0.0', resources: [{ name: '[variables(\'virtualMachineName\')]' }], variables: { virtualMachineName: 'VM-MultiNic' } };
       const tp = new TemplateParser(json);
 
       const result = tp.parseTemplate();
@@ -22,7 +21,7 @@ describe('parsers', () => {
 
       expect(result.schema).toBe('some_schema');
       expect(result.contentVersion).toBe('1.0.0.0');
-      expect(result.resources[0].displayName).toBe("VM-MultiNic");
+      expect(result.resources[0].displayName).toBe('VM-MultiNic');
     });
 
     it('should handle variables in dependsOn resources name', () => {
@@ -31,18 +30,18 @@ describe('parsers', () => {
         contentVersion: '1.0.0.0',
         resources: [
           {
-            name: "[variables('virtualMachineName')]",
+            name: '[variables(\'virtualMachineName\')]',
             dependsOn: [
-              "[variables('nic1')]",
-              "[variables('nic2')]",
-              "[variables('diagStorageAccountName')]"
+              '[variables(\'nic1\')]',
+              '[variables(\'nic2\')]',
+              '[variables(\'diagStorageAccountName\')]'
             ]
           }
         ],
         variables: {
-          nic1: "nic-1",
-          nic2: "nic-2",
-          diagStorageAccountName: "[concat('diags','some_unique_string')]"
+          nic1: 'nic-1',
+          nic2: 'nic-2',
+          diagStorageAccountName: '[concat(\'diags\',\'some_unique_string\')]'
         }
       };
       const tp = new TemplateParser(json);
@@ -52,9 +51,9 @@ describe('parsers', () => {
 
       expect(result.schema).toBe('some_schema');
       expect(result.contentVersion).toBe('1.0.0.0');
-      expect(result.resources[0].dependsOn[0].name).toBe("nic-1");
-      expect(result.resources[0].dependsOn[1].name).toBe("nic-2");
-      expect(result.resources[0].dependsOn[2].name).toBe("diagssome_unique_string");
+      expect(result.resources[0].dependsOn[0].name).toBe('nic-1');
+      expect(result.resources[0].dependsOn[1].name).toBe('nic-2');
+      expect(result.resources[0].dependsOn[2].name).toBe('diagssome_unique_string');
     });
   });
 });
