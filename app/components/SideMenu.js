9969385fb98afbab49443f3dc6e7a89258262469
Kamil Mrzygłód <kamz@demant-technology.com>
2017-08-28 13:04:02 +0200

    Added popups to describe menu items

--- app/components/SideMenu.js
+++ app/components/SideMenu.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { Icon, Menu } from 'semantic-ui-react';
+import { Icon, Menu, Popup } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import styles from './SideMenu.css'; // eslint-disable-line flowtype-errors/show-errors
 
@@ -13,12 +13,22 @@ export default class SideMenu extends Component {
   render() {
     return (
       <Menu fixed="left" icon inverted vertical className={styles.sideMenu}>
-        <Menu.Item active={this.props.currentView === '/'}>
-          <Link to="/"><Icon name="cloud" size="big" /></Link>
-        </Menu.Item>
-        <Menu.Item active={this.props.currentView === '/editor'}>
-          <Link to="/editor"><Icon name="write" size="big" /></Link>
-        </Menu.Item>
+        <Popup
+          trigger={<Menu.Item active={this.props.currentView === '/'}>
+            <Link to="/"><Icon name="cloud" size="big" /></Link>
+          </Menu.Item>}
+          content="Visualization"
+          position="right center"
+          size="mini"
+        />
+        <Popup
+          trigger={<Menu.Item active={this.props.currentView === '/editor'}>
+            <Link to="/editor"><Icon name="write" size="big" /></Link>
+          </Menu.Item>}
+          content="Editor"
+          position="right center"
+          size="mini"
+        />
         <Menu.Item onClick={() => this.props.openSettings()} active={this.props.currentView === 'Settings'} className={styles.settings}>
           <Icon name="settings" size="big" />
         </Menu.Item>
