02ed06f2b512e1141299ffeda52e2b4ba104b470
Kamil Mrzygłód <kamz@demant-technology.com>
2017-10-27 07:34:59 +0200

    Generation of an image of a resource moved to one place in `ImageGenerator` class

--- app/components/Visualization.js
+++ app/components/Visualization.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import Graph from 'react-graph-vis';
-import { remote } from 'electron';
+import ImageGenerator from '../resources/imageGenerator';
 import styles from './Visualization.css'; // eslint-disable-line flowtype-errors/show-errors
 
 export default class Visualization extends Component {
@@ -13,68 +13,6 @@ export default class Visualization extends Component {
   }
 
   resources: any;
-
-  static findImage(resourceType: string) {
-    let dir = '';
-    if (process.env.NODE_ENV === 'development') {
-      dir = '../assets/azure/';
-    } else {
-      dir = `${remote.app.getAppPath()}../../assets/azure/`;
-    }
-
-    switch (resourceType) {
-      case 'Microsoft.Web/serverfarms':
-        return `${dir}Azure App Service_COLOR.png`;
-      case 'Microsoft.Web/sites':
-        return `${dir}Azure App Service - Web App_COLOR.png`;
-      case 'Microsoft.Insights/components':
-        return `${dir}Azure Application Insights_COLOR.png`;
-      case 'Microsoft.Storage/storageAccounts':
-        return `${dir}Azure Storage.png`;
-      case 'Microsoft.EventHub/namespaces':
-        return `${dir}Azure Event Hubs_COLOR.png`;
-      case 'Microsoft.Network/trafficManagerProfiles':
-        return `${dir}Azure Traffic Manager_COLOR.png`;
-      case 'Microsoft.Network/trafficManagerProfiles/azureEndpoints':
-        return `${dir}Azure Traffic Manager.png`;
-      case 'Microsoft.NotificationHubs/namespaces':
-        return `${dir}Azure Notification Hubs_COLOR.png`;
-      case 'Microsoft.Resources/deployments':
-        return `${dir}Unidentified feature object_COLOR.png`;
-      case 'Microsoft.Network/networkInterfaces':
-        return `${dir}Azure Virtual Network.png`;
-      case 'Microsoft.Network/virtualNetworks':
-        return `${dir}Azure Virtual Network_COLOR.png`;
-      case 'Microsoft.Network/publicIpAddresses':
-        return `${dir}Azure Virtual Network.png`;
-      case 'Microsoft.Network/publicIPAddresses':
-        return `${dir}Azure Virtual Network.png`;
-      case 'Microsoft.Network/networkSecurityGroups':
-        return `${dir}Azure Virtual Network.png`;
-      case 'Microsoft.Compute/virtualMachines':
-        return `${dir}Azure Virtual Machine_COLOR.png`;
-      case 'Microsoft.Network/applicationGateways':
-        return `${dir}Azure Application Gateway_COLOR.png`;
-      case 'Microsoft.Search/searchServices':
-        return `${dir}Azure Search_COLOR.png`;
-      case 'Microsoft.ContainerService/containerServices':
-        return `${dir}Azure Container Service_COLOR.png`;
-      case 'Microsoft.Web/sites/domainOwnershipIdentifiers':
-        return `${dir}Azure App Service - Web App (was Websites).png`;
-      case 'Microsoft.Automation/automationAccounts':
-        return `${dir}Azure Automation_COLOR.png`;
-      case 'Microsoft.Compute/availabilitySets':
-        return `${dir}Azure Virtual Machines - Availability Set_COLOR.png`;
-      case 'Microsoft.ApiManagement/service':
-        return `${dir}Azure API Management_COLOR.png`;
-      case 'Microsoft.Backup/BackupVault/registeredContainers/protectedItems':
-        return `${dir}Azure Backup_COLOR.png`;
-      case 'Microsoft.Backup/BackupVault':
-        return `${dir}Azure Backup - Recovery Vault.png`;
-      default:
-        return `${dir}Unidentified feature object_COLOR.png`;
-    }
-  }
 
   render() {
     const props = this.props;
@@ -99,7 +37,7 @@ export default class Visualization extends Component {
         label: resource.displayName,
         title: resource.displayName,
         shape: 'image',
-        image: Visualization.findImage(this.resources[i].type)
+        image: ImageGenerator.findImage(this.resources[i].type)
       });
     }
 
