7726cfe3f95b1a089485a8dc8a404a2797528fa7
Kamil Mrzygłód <kamz@demant-technology.com>
2018-04-05 12:48:47 +0200

    Revert "QuickTemplates 1/3 (#83)"
    This reverts commit 8dc2156e7d028d6a245387651b42f0094ea0bdb0.

--- _agt_clean.ps1
+++
@@ -1,7 +0,0 @@
-$rootPath = split-path -parent $MyInvocation.MyCommand.Path;
-Write-Output($rootPath);
-
-$templatesSubdirs = "$($rootPath)\azure-quickstart-templates\";
-
-Get-ChildItem -Path "$($templatesSubdirs)" -Exclude "*.json"  -File -Recurse | ? { $_.FullName -notmatch '.github' } | foreach { ($_.Delete()); }
-Write-Output("Removed");
